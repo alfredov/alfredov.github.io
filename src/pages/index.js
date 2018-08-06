98a0e16629874b5632adb350a73ba773eb57827a
Alfredo V <alfredo.villagomez.m@gmail.com>
2018-08-06 12:59:03 -0400

    Setting first boilerplate with gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my personal site.</p>
+    <p>I'm building a webapp where I'll share all my knowledge.</p>
+  </div>
+)
+
+export default IndexPage
